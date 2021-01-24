04c1ef443af4957e1c9cbc2c361cc4283e011272
unknown <sscott@divergent3d.com>
2021-01-24 12:32:49 -0800

    conan fix

--- conanfile.py
+++ conanfile.py
@@ -1,11 +1,11 @@
 from conans import ConanFile, CMake
 
-class PocoTimerConan(ConanFile):
+class SvgeezerConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
     requires = "opencv/4.5.1", \
                 "boost/1.75.0"
     generators = "cmake", "gcc", "txt"
-    default_options = {"opencv:shared": True, "boost:shared": True}
+    # default_options = {"opencv:shared": True, "boost:shared": True}
 
     # def imports(self):
     #     self.copy("*.dll", dst="bin", src="bin") # From bin to bin
